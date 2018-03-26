ba48cc9f2ca7c18546c6ecec1750583407822271
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-26 20:45:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,14 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/recipes/new' do 
+      erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name:params[:name], ingredients:params[:ingredients], cook_time: params[:cook_time])
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :recipes
